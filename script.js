953114edce6f9a546305efe1115141fe27660dd2
Alin Cola <colaalincristian@gmail.com>
2022-11-15 12:01:36 +0200

    delete button

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
- let all = document.querySelector("#all");
- let open = document.querySelector("#open");
- let done = document.querySelector("#done");
+let all = document.querySelector("#all");
+let open = document.querySelector("#open");
+let done = document.querySelector("#done");
 
- all.addEventListener("click", control);
- open.addEventListener("click", control);
- done.addEventListener("click", control);
+all.addEventListener("click", control);
+open.addEventListener("click", control);
+done.addEventListener("click", control);
 
- let checkBoxCount = 0;
+let checkBoxCount = 0;
 
- let lastClickedElement
+let lastClickedElement;
 
- function control(event) {
-     if (event.target.checked) {
-         checkBoxCount++
-     } else {
-         checkBoxCount--
-     }
-     if (checkBoxCount === 2) {
-         checkBoxCount--
-         lastClickedElement.checked = false;
-     }
-     lastClickedElement = event.target;
- }
+function control(event) {
+  if (event.target.checked) {
+    checkBoxCount++;
+  } else {
+    checkBoxCount--;
+  }
+  if (checkBoxCount === 2) {
+    checkBoxCount--;
+    lastClickedElement.checked = false;
+  }
+  lastClickedElement = event.target;
+}
 
 // ort wo man die todos speichern kann
 
@@ -44,7 +44,7 @@ const addBtn = document.querySelector("#add");
 addBtn.addEventListener("click", (event) => {
   // function addTodo ausführen
   addTodo();
-    render();
+  render();
 });
 // text ausgeben
 
@@ -55,6 +55,24 @@ function render() {
   for (let i = 0; i < todos.length; i++) {
     const li = document.createElement("li");
     li.innerText = todos[i].description;
-      todoList.appendChild(li);
+    todoList.appendChild(li);
   }
 }
+
+// function delete
+
+function deleteInput() {
+  const output = document.querySelector("#new-todo");
+  todos.splice("");
+}
+
+// delete button erzeugen
+
+const delBtn = document.querySelector("#btn-delete");
+
+//function delete ausführen
+
+delBtn.addEventListener("click", (e) => {
+  deleteInput();
+  render();
+});
